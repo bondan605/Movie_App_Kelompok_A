f1c9b239b3718d468fb8900978c2c6ff7d10f2a3
herdiyana256 <herdiyanadam256@gmail.com>
2024-07-02 00:09:19 +0700

    add searchBar, GenreScreen, Api, MovieScreen

--- App.tsx
+++ App.tsx
@@ -31,13 +31,16 @@ import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import BottomTabNavigator from "./src/navigations/BottomTabNavigation";
 import SearchScreen from "./src/screens/SearchScreen";
+import GenreScreen from "./src/screens/GenreScreen";
+import MovieScreen from "./src/screens/MovieScreen";
 
 const App = (): JSX.Element => {
   return (
     <NavigationContainer>
       <BottomTabNavigator.Navigator>
         <BottomTabNavigator.Screen name="Home" component={SearchScreen} />
-        {/* Tambahkan layar lainnya di sini */}
+        <BottomTabNavigator.Screen name="Genre" component={GenreScreen} />
+        <BottomTabNavigator.Screen name="Movie" component={MovieScreen} />
       </BottomTabNavigator.Navigator>
     </NavigationContainer>
   );
