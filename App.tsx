91f7d51aa958fb57729297620684b1d19981a03a
Ahmad Bondan Gunawan <73745874+bondan605@users.noreply.github.com>
2024-07-01 19:18:33 +0700

    Revert "Menambahkan fitur pencarian pada aplikasi film"

--- App.tsx
+++ App.tsx
@@ -10,35 +10,14 @@
 //   );
 // }
 
-// import React from "react";
-// import { NavigationContainer } from "@react-navigation/native";
-// import BottomTabNavigator from "./src/navigations/BottomTabNavigation";
-
-// const App = (): JSX.Element => {
-//   return (
-//     <NavigationContainer>
-//       <BottomTabNavigator />
-//     </NavigationContainer>
-//   );
-// };
-
-// export default App;
-
-
-
-//CHANGED
 import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import BottomTabNavigator from "./src/navigations/BottomTabNavigation";
-import SearchScreen from "./src/screens/SearchScreen";
 
 const App = (): JSX.Element => {
   return (
     <NavigationContainer>
-      <BottomTabNavigator.Navigator>
-        <BottomTabNavigator.Screen name="Home" component={SearchScreen} />
-        {/* Tambahkan layar lainnya di sini */}
-      </BottomTabNavigator.Navigator>
+      <BottomTabNavigator />
     </NavigationContainer>
   );
 };
